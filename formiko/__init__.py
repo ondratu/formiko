9e82d11202767fd7e07ba866c8b57e60a946ad1e
Ondrej Tuma <mcbig@zeropage.cz>
2017-10-10 18:23:41 +0200

    rename version to 1.1.1.dev0

--- formiko/__init__.py
+++ formiko/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "1.1.1dev"
+__version__ = "1.1.1.dev0"
 __copyright__ = "(c) 2016"
 __author__ = "Ondřej Tůma <mcbig@zeropage.cz>"
 __url__ = "https://github.com/ondratu/formiko"
