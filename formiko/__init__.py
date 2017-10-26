6f82fb9e902b1e4887309ff9aab430bad148be96
Ondrej Tuma <mcbig@zeropage.cz>
2017-10-26 19:41:39 +0200

    release version

--- formiko/__init__.py
+++ formiko/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "1.2.0.dev0"
-__copyright__ = "(c) 2016"
+__version__ = "1.2.0"
+__copyright__ = "(c) 2017"
 __author__ = "Ondřej Tůma <mcbig@zeropage.cz>"
 __url__ = "https://github.com/ondratu/formiko"
 __comment__ = \
