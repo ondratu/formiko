25742c645632ba085eddd105a130632ab6b50869
Ondrej Tuma <mcbig@zeropage.cz>
2024-11-14 23:20:28 +0100

    manifest update

--- formiko/__init__.py
+++ formiko/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "1.5.0"
-__copyright__ = "(c) 2018"
+__copyright__ = "(c) 2024"
 __author__ = "Ondřej Tůma <mcbig@zeropage.cz>"
 __url__ = "https://github.com/ondratu/formiko"
 __comment__ = \
